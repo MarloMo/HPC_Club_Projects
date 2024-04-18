2e867c22f7c7f99902725c71b8d2082e568fe9a8
Marlo Morales <moralesmarlo486@gmail.com>
2024-04-17 20:14:12 -0700

    add pi example

---
+++ Code_Examples/PI_Calc/serial_pi.py
@@ -0,0 +1,44 @@
+import numpy as np
+import random as rnd
+from matplotlib import pyplot as plt
+
+
+def estimatePI(N):
+    '''
+    N: Hits + Misses (Total number of throws)
+    '''
+
+    result = []
+
+    for i in range(len(N)):
+        # M: Hits (starting from 0)
+        M = 0
+        for j in range(int(N[i])):
+
+            x = rnd.random()
+            y = rnd.random()
+            r_squared = x * x + y * y
+
+            if (r_squared < 1):
+                M = M + 1
+        Pi = 4.0 * M / N[i]
+        result.append(Pi)
+
+    return result
+
+
+N = [1e1, 1e2, 1e3, 1e4, 1e5, 1e6, 1e7]
+
+Pi_results = estimatePI(N)
+
+diff_list = []
+for i in range(len(Pi_results)):
+    diff_list.append(np.abs(Pi_results[i] - np.pi))
+
+plt.plot(np.array(N), diff_list, label="abs(estimatePi - Pi)")
+plt.legend()
+plt.xscale('log')
+plt.xlabel("number of throws")
+plt.yscale('log')
+plt.ylabel("convergence")
+plt.savefig("convergence_test.pdf")
